a8533eda04ff118d45c34ed947d1a78ecd2a86b2
alexborodach30 <sagopien04@gmail.com>
2025-04-18 19:49:45 +0300

    Fixed JS for create.

--- MoneyProject/RecordApp/views.py
+++ MoneyProject/RecordApp/views.py
@@ -150,6 +150,7 @@ class RecordCreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["types"] = TransactionType.objects.all()
         context["subcategories"] = SubCategory.objects.all()
         context["categories"] = Category.objects.all()
         return context
